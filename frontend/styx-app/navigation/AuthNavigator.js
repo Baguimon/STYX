b5523fbddf0d87c38f5dbf473468de66c4274d30
Yoann <yoyolc.2004@gmail.com>
2025-05-04 20:21:40 -0400

    écran intro + refonte login & register Screens

--- frontend/styx-app/navigation/AuthNavigator.js
+++ frontend/styx-app/navigation/AuthNavigator.js
@@ -1,10 +1,9 @@
+// navigation/AuthNavigator.js
 import React, { useContext } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import LoginScreen from '../screens/LoginScreen';
 import RegisterScreen from '../screens/RegisterScreen';
-import BottomTabNavigator from './BottomTabNavigator'; 
 import { AuthContext } from '../contexts/AuthContext';
 
 const Stack = createNativeStackNavigator();
@@ -12,22 +11,16 @@ const Stack = createNativeStackNavigator();
 export default function AuthNavigator() {
   const { isAuthenticated } = useContext(AuthContext);
 
-  if (isAuthenticated === null) return null; 
+  // On n'affiche rien tant que l'authentification n'est pas déterminée
+  if (isAuthenticated === null) return null;
+
+  // Si déjà connecté, RootNavigator gère la redirection vers "Main"
+  if (isAuthenticated) return null;
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName={isAuthenticated ? 'Main' : 'Login'} screenOptions={{ headerShown: false }}>
-        {!isAuthenticated ? (
-          <>
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="Register" component={RegisterScreen} />
-          </>
-        ) : (
-          <>
-            <Stack.Screen name="Main" component={BottomTabNavigator} />
-          </>
-        )}
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
+    </Stack.Navigator>
   );
 }
