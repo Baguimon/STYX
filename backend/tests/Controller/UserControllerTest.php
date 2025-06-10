40f804a617f93ffdade1fb22b9504fa797cf7ef9
Baguimon <timothe.winkler@gmail.com>
2025-06-09 20:31:47 -0400

    Test Controller à fixer

---
+++ backend/tests/Controller/UserControllerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\Controller\UserController;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class UserControllerTest extends TestCase
+{
+    public function testListReturnsUsers()
+    {
+        $user1 = new User();
+        $user1->setEmail('a@example.com');
+        $user1->setUsername('A');
+        $user1->setCreatedAt(new \DateTime('2023-01-01 10:00:00'));
+        $user1->setRole('ROLE_USER');
+        $user1->setLevel('Beginner');
+
+        $repo = $this->createMock(UserRepository::class);
+        $repo->expects($this->once())->method('findAll')->willReturn([$user1]);
+
+        $controller = new UserController();
+        $response = $controller->list($repo);
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $data = json_decode($response->getContent(), true);
+        $this->assertCount(1, $data);
+        $this->assertSame('a@example.com', $data[0]['email']);
+    }
+}
